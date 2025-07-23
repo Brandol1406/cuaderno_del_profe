bb391d08b279b8764640bb9faf891e2f2698b359
Carlos Brandol Lora Geraldino <clora@contraloria.gob.do>
2025-07-23 15:08:15 -0400

    haciendo la funcion de filtrar un filtro generico

--- cuaderno_del_profe.frontend/src/app/pages/estudiantes/estudiantes-list.component.ts
+++ cuaderno_del_profe.frontend/src/app/pages/estudiantes/estudiantes-list.component.ts
@@ -31,11 +31,11 @@ export class EstudiantesListComponent implements OnInit {
     const lower = this.searchText.toLowerCase();
 
     return this.items.filter(item =>
-      // Object.keys(item).some(key => item[key].toString().toLowerCase().includes(lower))
+      Object.keys(item).some(key => (item[key] ?? '').toString().toLowerCase().includes(lower))
 
-      item.matricula?.toLowerCase().includes(lower) ||
-      item.nombres?.toLowerCase().includes(lower) ||
-      item.apellidos?.toLowerCase().includes(lower)
+      //item.matricula?.toLowerCase().includes(lower) ||
+      //item.nombres?.toLowerCase().includes(lower) ||
+      //item.apellidos?.toLowerCase().includes(lower)
     );
   }
 
